80e7e0c70e67836a97e349b485d25be1ebf0f834
Grigoris Chatzinikolaou <greghatzis@gmail.com>
2025-11-10 10:32:47 +0200

    Update config

---
+++ rollup.config.js
@@ -0,0 +1,68 @@
+import pkg from './package.json' with { type: 'json' };
+import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import cleanup from 'rollup-plugin-cleanup';
+import babel from '@rollup/plugin-babel';
+
+
+export default [
+	// browser-friendly UMD build
+	{
+		input: 'src/index.js',
+		output: {
+			name: 'AppBlock',
+			file: pkg.browser,
+			format: 'umd',
+			sourcemap: true
+		},
+		plugins: [
+			commonjs(),
+			resolve({browser: true}),
+			cleanup(),
+			babel({
+				exclude: 'node_modules/**',
+				babelHelpers: "bundled"
+			})
+		]
+  },
+
+  // Minified version
+  {
+    input: 'src/index.js',
+    output: {
+		name: 'AppBlock',
+		file: pkg.minified,
+		format: 'umd',
+		sourcemap: true
+    },
+		plugins: [
+			commonjs(),
+			resolve({browser: true}),
+			cleanup(),
+			babel({
+				exclude: 'node_modules/**',
+				babelHelpers: "bundled"
+			}),
+			terser(),
+		]
+	},
+
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	{
+		input: 'src/index.js',
+		output: [
+			{ file: pkg.main, format: 'cjs', exports: 'default' },
+			{ file: pkg.module, format: 'es' }
+		],
+		plugins: [
+			resolve(),
+			commonjs(),
+			cleanup(),
+			babel({
+				exclude: 'node_modules/**',
+				babelHelpers: "bundled"
+			})
+		]
+	}
+];
